68a2493e447fdf76b5b640e8dec44b2801c27328
goonan24 <jgoonan24@gmail.com>
2024-05-10 18:22:51 -0400

    commit

---
+++ src/app/Components/footer.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+    return (
+      <main> 
+        <div className="grid grid-cols-3 gap-4 text-left align-baseline bg-sky-950 top-padding-500">
+            <div className="">
+              <h2>RVN Services</h2>
+              <p>Site designed and maintained by RVN Services. For site related inquiries, contact jgoonan24@gmail.com</p>
+            </div>
+            <div id = "block2">
+              <h2>Contact</h2>
+              <p>1117 Edison Ave.,</p>
+              <p>Bronx, N.Y. 10461</p>
+              <p>                         </p>
+              <p>Email: jgoonan24@gmail.com</p>
+              <p>Phone: (347) 261-2903</p>
+            </div>
+        </div>
+       
+    </main>
+    );
+  }
--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -2,10 +2,13 @@ export default function Home() {
     return (
       
         <main>
-          <div className="text-2xl text-center bg-blue-500">
-            <h1 is-upgraded="" className="title">
-              <a href="https://homecoming.psu.edu/">Homecoming OPP Inventory</a>
+          <div className="text-center bg-orange-600">
+            <h1 className="text-2xl">
+              <a href="https://homecoming.psu.edu/">RVN Services</a>
             </h1>
+            <h2 className="text-xl">
+              <a>24/7 Service for the Tri-State Area</a>
+            </h2>
           </div>
           <div className="grid grid-cols-3 gap-4">
             <button className="hover:bg-cyan-600">Add To Inventory</button>
