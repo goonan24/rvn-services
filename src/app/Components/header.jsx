9dc4e0974a9d87a9eba520c9916982c79e0ceb72
goonan24 <jgoonan24@gmail.com>
2024-05-10 21:36:10 -0400

    commit

--- src/app/Components/header.jsx
+++ src/app/Components/header.jsx
@@ -10,7 +10,7 @@ export default function Home() {
               <a>24/7 Service for the Tri-State Area</a>
             </h2>
           </div>
-          <div className="text-center">
+          <div className="text-center bg-orange-600">
             <a href="https://forms.gle/xXAN1W5zPhgNR8MM9">
             <button className="hover:bg-cyan-600">Please Leave a Review</button>
             </a>
