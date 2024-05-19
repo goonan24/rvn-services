41e82ce128345490cdf2aef0eb286b63a37bc6b9
goonan24 <jgoonan24@gmail.com>
2024-05-19 16:16:54 -0400

    commit

--- src/app/Components/footer.jsx
+++ src/app/Components/footer.jsx
@@ -4,15 +4,16 @@ export default function Home() {
         <div className="grid grid-cols-3 gap-4 text-left align-baseline bg-orange-600">
             <div className="">
               <h2>RVN Services</h2>
-              <p>Site designed and maintained by RVN Services. For site related inquiries, contact jgoonan24@gmail.com</p>
+              <p className="text-sm">Site designed and maintained by RVN Services.</p>
+              <p className="text-sm">For site related inquiries, contact jgoonan24@gmail.com</p>
             </div>
             <div id = "block2">
               <h2>Contact</h2>
-              <p>1117 Edison Ave.,</p>
-              <p>Bronx, N.Y. 10461</p>
+              <p className="text-sm">1117 Edison Ave.,</p>
+              <p className="text-sm">Bronx, N.Y. 10461</p>
               <p>                         </p>
-              <p>Email: jgoonan24@gmail.com</p>
-              <p>Phone: (347) 261-2903</p>
+              <p className="text-sm">Email: jgoonan24@gmail.com</p>
+              <p className="text-sm">Phone: (347) 261-2903</p>
             </div>
             <div>
                 <h2>Other Links</h2>
